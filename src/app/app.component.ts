72527f340a5412c27facd077e712c6489b14e82f
Jacob Allen <jedijake@me.com>
2017-03-09 23:37:16 -0800

    complete Angular 2 De-sugar video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   private switch = true;
   private items = [1, 2, 3, 4, 5];
+  private value = 10;
   onSwitch() {
     this.switch = !this.switch;
   }
